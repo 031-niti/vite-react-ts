b88ee852ceeaa16318e63e99a1f489412b09044f
Niti <earthza6263@gmail.com>
2023-11-14 13:57:26 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BMICalculatorComponent from './components/BMICalculatorComponent'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <BMICalculatorComponent/>
+    </>
+  )
+}
+
+export default App
